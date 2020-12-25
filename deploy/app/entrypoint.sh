c3d9f4ca82ab8cc65d761907cc8485183471e02b
alfreedom <alfredoopa@gmail.com>
2020-12-25 14:59:26 -0600

    Add START_APP env var for test pipeline

--- deploy/app/entrypoint.sh
+++ deploy/app/entrypoint.sh
@@ -3,8 +3,17 @@
 # Wait for postgres on host "db" and port "5432"
 /wait-for-it.sh db:5432
 
-# Once postgres has started, run migrations
-python manage.py migrate
+django_migrate=${DJANGO_MIGRATE-no}
+if [ "$django_migrate" == 'yes' ]; then
+  # Once postgres has started, run migrations
+  python manage.py migrate
+  fi
 
-# Finally run the app with supervisor daemon using default.conf and including pos.supervisor.conf
-/usr/bin/supervisord -n
+start_app=${START_APP-yes}
+if [ "$start_app" == 'yes' ]; then
+  # Finally run the app with supervisor daemon using default.conf and including pos.supervisor.conf
+  /usr/bin/supervisord -n
+fi
+
+echo "$@"
+exec "$@" 1>&2
