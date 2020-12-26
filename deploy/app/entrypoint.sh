65da707b5380e441282531e4f830fa5079b1f0ab
alfreedom <alfredoopa@gmail.com>
2020-12-25 18:25:36 -0600

    add build and push docker image to docker hub

--- deploy/app/entrypoint.sh
+++ deploy/app/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Wait for postgres on host "db" and port "5432"
-/wait-for-it.sh db:5432
+/wait-for-it.sh ${DB_HOST}:5432
 
+# Check if need migrations
 django_migrate=${DJANGO_MIGRATE-False}
 if [ "$django_migrate" == 'True' ]; then
-  # Once postgres has started, run migrations
   python manage.py migrate
   fi
 
+# Run the app with supervisor daemon using default.conf and including pos.supervisor.conf
 start_app=${START_APP-True}
 if [ "$start_app" == 'True' ]; then
-  # Finally run the app with supervisor daemon using default.conf and including pos.supervisor.conf
   /usr/bin/supervisord -n
 fi
 
+# Run conmmand passed to argument script
 echo "$@"
 exec "$@" 1>&2
--- deploy/app/gunicorn_conf.py
+++ deploy/app/gunicorn_conf.py
@@ -1,5 +1,4 @@
 import multiprocessing
 
 bind = '0.0.0.0:8000'
-workers = 3
 workers = multiprocessing.cpu_count() * 2 + 1
