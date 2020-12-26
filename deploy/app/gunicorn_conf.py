55b18688ddd84387625c9a5f0f2e22b3e3066a94
alfreedom <alfredoopa@gmail.com>
2020-12-25 23:58:56 -0600

    Update heroku deployment with postgres

--- deploy/app/gunicorn_conf.py
+++ deploy/app/gunicorn_conf.py
@@ -2,5 +2,5 @@ import multiprocessing
 from os import environ as env
 
 
-bind = '0.0.0.0:{}'.format(env.get('APP_PORT', '8080'))
+bind = '0.0.0.0:{}'.format(env.get('PORT', '8080'))
 workers = multiprocessing.cpu_count() * 2 + 1
