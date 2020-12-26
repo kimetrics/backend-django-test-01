f6b51e5f0039cd7c1a92e7a0ae872c3b844d73ee
alfreedom <alfredoopa@gmail.com>
2020-12-25 22:58:49 -0600

    Update heroku deployment with postgres

--- deploy/app/entrypoint.sh
+++ deploy/app/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Wait for postgres on host "db" and port "5432"
-/wait-for-it.sh ${DB_HOST}:5432
+# Wait for postgres
+/wait-for-it.sh ${DB_HOST}:${DB_PORT}
 
 # Check if need migrations
 django_migrate=${DJANGO_MIGRATE-False}
 if [ "$django_migrate" == 'True' ]; then
   python manage.py migrate
-  fi
+fi
 
 # Run the app with supervisor daemon using default.conf and including pos.supervisor.conf
 start_app=${START_APP-True}
--- deploy/app/gunicorn_conf.py
+++ deploy/app/gunicorn_conf.py
@@ -1,4 +1,6 @@
 import multiprocessing
+from os import environ as env
 
-bind = '0.0.0.0:8000'
+
+bind = '0.0.0.0:{}'.format(env.get('APP_PORT', '8080'))
 workers = multiprocessing.cpu_count() * 2 + 1
