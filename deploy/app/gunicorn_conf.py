d42150da51b1c81bc4fc667e27408427012cb351
alfreedom <alfredoopa@gmail.com>
2020-12-27 14:41:32 -0600

    refactor dockerhub repository and add gunicorn workers environment var

--- deploy/app/gunicorn_conf.py
+++ deploy/app/gunicorn_conf.py
@@ -3,4 +3,5 @@ from os import environ as env
 
 
 bind = ':{}'.format(env.get('PORT', '8080'))
-workers = multiprocessing.cpu_count() * 2 + 1
+env_workers = env.get('GUNICORN_WORKERS', None)
+workers = env_workers if env_workers else multiprocessing.cpu_count() * 2 + 1
