4392f169fb7649d703e474ae57bbf6368831b747
alfreedom <alfredoopa@gmail.com>
2020-12-27 22:06:30 -0600

    Fix flake8 linting

--- src/pos/urls.py
+++ src/pos/urls.py
@@ -1,8 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 from modules.inventory.views import ProductApiViewSet
 from modules.orders.views import OrderApiViewSet
